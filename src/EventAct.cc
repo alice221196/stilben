13356a6877e3feb95f7648a5633aec3f4de0cbf4
Kaf24 <47420439+Kaf24@users.noreply.github.com>
2019-02-22 13:43:06 +0300

    update

--- src/EventAct.cc
+++ src/EventAct.cc
@@ -7,9 +7,18 @@ EventAct::EventAct() {
 }
 EventAct::~EventAct() {}
 
-void EventAct::BeginOfEventAction(const G4Event * EVE){
-	//G4cout << "BeginWorks\t" << EVE->GetEventID() << G4endl;
+int SLcounter=0;
+
+void EventAct::StepLengthCounter(G4double count1) { //описание функции приема значения из шага
+	SLcounter ++;
+	G4cout << "Step N= " << SLcounter << "\t, Length=" << count1 << G4endl; //debug-инфо.
 };
 
-void EventAct::EndOfEventAction(const G4Event *EVE){
+void EventAct::BeginOfEventAction(const G4Event * EVE) {
+	//G4cout << "BeginWorks\t" << EVE->GetEventID() << G4endl;
+	SLcounter = 0; //обнуление счетчика каждый раз при запуске единичного события
+};
+
+void EventAct::EndOfEventAction(const G4Event *EVE) {
+		
 };
--- src/StepAction.cc
+++ src/StepAction.cc
@@ -1,6 +1,11 @@
 #include <StepAction.hh>
+#include <EventAct.hh>//подключаем библиотеку с используемым счетчиком
 
 // Обработчик событий на уровне Step - просчет отдельного шага/акта взаимодействия
 
 void StepAct::UserSteppingAction(const G4Step* step) {
+
+	G4double StepLength = step->GetStepLength(); //извлекаем инфу из шага
+	EventAct::StepLengthCounter(StepLength);	//передаем инфу в другой класс (на уровень выше)
+
 }
